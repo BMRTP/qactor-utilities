445e9704cb634a9bf408d47375c3c8d094068e60
Edoardo Barbieri <edoardo.barbieri4@studio.unibo.it>
2020-01-30 14:27:32 +0100

    First commit

---
+++ QActorContextInterface/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace QActorContextInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbMessage = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbMessageID = new System.Windows.Forms.TextBox();
+            this.txbMit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbMessageType = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.txbDest = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.txbPort = new System.Windows.Forms.TextBox();
+            this.txbIPAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnSend2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMessage = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 217);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Message:";
+            // 
+            // txbMessage
+            // 
+            this.txbMessage.Location = new System.Drawing.Point(98, 214);
+            this.txbMessage.Name = "txbMessage";
+            this.txbMessage.Size = new System.Drawing.Size(164, 20);
+            this.txbMessage.TabIndex = 34;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 191);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Message ID:";
+            // 
+            // txbMessageID
+            // 
+            this.txbMessageID.Location = new System.Drawing.Point(98, 188);
+            this.txbMessageID.Name = "txbMessageID";
+            this.txbMessageID.Size = new System.Drawing.Size(164, 20);
+            this.txbMessageID.TabIndex = 32;
+            // 
+            // txbMit
+            // 
+            this.txbMit.Location = new System.Drawing.Point(98, 162);
+            this.txbMit.Name = "txbMit";
+            this.txbMit.Size = new System.Drawing.Size(164, 20);
+            this.txbMit.TabIndex = 29;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Mit:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Dest:";
+            // 
+            // cmbMessageType
+            // 
+            this.cmbMessageType.FormattingEnabled = true;
+            this.cmbMessageType.Items.AddRange(new object[] {
+            "event",
+            "dispatch",
+            "request",
+            "response"});
+            this.cmbMessageType.Location = new System.Drawing.Point(98, 109);
+            this.cmbMessageType.Name = "cmbMessageType";
+            this.cmbMessageType.Size = new System.Drawing.Size(164, 21);
+            this.cmbMessageType.TabIndex = 26;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Message Type:";
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.Location = new System.Drawing.Point(268, 12);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(321, 329);
+            this.lstOutput.TabIndex = 24;
+            // 
+            // txbDest
+            // 
+            this.txbDest.Location = new System.Drawing.Point(98, 136);
+            this.txbDest.Name = "txbDest";
+            this.txbDest.Size = new System.Drawing.Size(164, 20);
+            this.txbDest.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Port:";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(15, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(58, 13);
+            this.lblIP.TabIndex = 21;
+            this.lblIP.Text = "IPAddress:";
+            // 
+            // txbPort
+            // 
+            this.txbPort.Location = new System.Drawing.Point(79, 38);
+            this.txbPort.Name = "txbPort";
+            this.txbPort.Size = new System.Drawing.Size(183, 20);
+            this.txbPort.TabIndex = 20;
+            this.txbPort.Text = "8018";
+            // 
+            // txbIPAddress
+            // 
+            this.txbIPAddress.Location = new System.Drawing.Point(79, 12);
+            this.txbIPAddress.Name = "txbIPAddress";
+            this.txbIPAddress.Size = new System.Drawing.Size(183, 20);
+            this.txbIPAddress.TabIndex = 19;
+            this.txbIPAddress.Text = "127.0.0.1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(15, 64);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(247, 23);
+            this.btnConnect.TabIndex = 18;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 240);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(247, 23);
+            this.btnSend.TabIndex = 37;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnSend2
+            // 
+            this.btnSend2.Location = new System.Drawing.Point(12, 318);
+            this.btnSend2.Name = "btnSend2";
+            this.btnSend2.Size = new System.Drawing.Size(250, 23);
+            this.btnSend2.TabIndex = 38;
+            this.btnSend2.Text = "Send";
+            this.btnSend2.UseVisualStyleBackColor = true;
+            this.btnSend2.Click += new System.EventHandler(this.btnSend2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Complete message:";
+            // 
+            // cmbMessage
+            // 
+            this.cmbMessage.FormattingEnabled = true;
+            this.cmbMessage.Location = new System.Drawing.Point(12, 291);
+            this.cmbMessage.Name = "cmbMessage";
+            this.cmbMessage.Size = new System.Drawing.Size(250, 21);
+            this.cmbMessage.TabIndex = 41;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 350);
+            this.Controls.Add(this.cmbMessage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnSend2);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txbMessage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txbMessageID);
+            this.Controls.Add(this.txbMit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbMessageType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.txbDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.txbPort);
+            this.Controls.Add(this.txbIPAddress);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "QActor Context Interface";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbMessage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbMessageID;
+        private System.Windows.Forms.TextBox txbMit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbMessageType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstOutput;
+        private System.Windows.Forms.TextBox txbDest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.TextBox txbPort;
+        private System.Windows.Forms.TextBox txbIPAddress;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnSend2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbMessage;
+    }
+}
+
---
+++ QActorContextInterface/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QActorContextInterface
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            btnSend2.Enabled = btnSend.Enabled = false;
+
+            if (File.Exists("commands.txt"))
+            {
+                cmbMessage.Items.AddRange(File.ReadAllLines("commands.txt"));
+            }
+        }
+
+        private int id = 1;
+        private void AddOutput(string val)
+        {
+            lstOutput.Items.Add(val);
+            int nItems = (int)(lstOutput.Height / lstOutput.ItemHeight);
+            lstOutput.TopIndex = lstOutput.Items.Count - nItems;
+        }
+        private void Read()
+        {
+            string v = "";
+            while (true)
+            {
+                try
+                {
+                    while (!v.Contains("\n"))
+                    {
+                        byte[] buffer = new byte[1024];
+                        int read = client.GetStream().Read(buffer, 0, 1024);
+                        v += Encoding.UTF8.GetString(buffer, 0, read);
+                    }
+
+                    int col = v.IndexOf('\n');
+                    string msg = v.Substring(0, col);
+                    lstOutput.Invoke(new MethodInvoker(delegate ()
+                    {
+                        AddOutput(msg);
+                    }));
+                    if (v.Length > col + 1)
+                    {
+                        v = v.Substring(col + 1, v.Length - col - 1);
+                    }
+                    else
+                    {
+                        v = "";
+                    }
+                }
+                catch
+                {
+                    v = "";
+                }
+            }
+        }
+
+        Thread readT;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (client == null)
+            {
+                try
+                {
+                    IPEndPoint target = new IPEndPoint(IPAddress.Parse(txbIPAddress.Text), int.Parse(txbPort.Text));
+
+                    client = new TcpClient();
+                    client.Connect(target);
+                    btnSend2.Enabled = btnSend.Enabled = true;
+                    btnConnect.Text = "Disconnect";
+                    AddOutput("Connected");
+                    readT = new Thread(Read);
+                    readT.Start();
+                }
+                catch (Exception ex)
+                {
+                    AddOutput(ex.Message);
+                }
+            }
+            else
+            {
+                CloseConnection();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            try
+            {
+                if (readT != null)
+                    readT.Abort();
+                if (readT != null)
+                    client.Close();
+            }
+            catch { }
+            btnSend2.Enabled = btnSend.Enabled = false;
+            btnConnect.Text = "Connect";
+            client = null;
+            AddOutput("Disconnected");
+        }
+
+        private void SendMsg(string msg)
+        {
+            if (!cmbMessage.Items.Contains(msg))
+                cmbMessage.Items.Add(msg);
+
+            List<string> commands = new List<string>();
+            foreach (var i in cmbMessage.Items)
+            {
+                commands.Add(i.ToString());
+            }
+            File.WriteAllLines("commands.txt", commands.ToArray());
+
+            try
+            {
+                AddOutput("Sent: " + msg);
+                byte[] data = Encoding.UTF8.GetBytes(msg + "\n");
+
+                client.GetStream().Write(data, 0, data.Length);
+                client.GetStream().Flush();
+            }
+            catch (Exception ex)
+            {
+                AddOutput(ex.Message);
+            }
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string msg = String.Format("msg({0},{1},{2},{3},{4},{5})",
+            txbMessageID.Text, cmbMessageType.Text, txbMit.Text, txbDest.Text, txbMessage.Text, id++);
+            SendMsg(msg);
+        }
+
+        private void btnSend2_Click(object sender, EventArgs e)
+        {
+            SendMsg(cmbMessage.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseConnection();
+        }
+    }
+}
